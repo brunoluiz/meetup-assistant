4745a47208929b7bbbaa9d2cc88b7b168b1de7c8
Bruno Luiz Silva <contact@brunoluiz.net>
2023-07-02 17:24:58 +0100

    docs: document

--- internal/templater/source/source.go
+++ internal/templater/source/source.go
@@ -7,6 +7,9 @@ import (
 	"github.com/hairyhenderson/go-fsimpl/gitfs"
 )
 
+// New parse the DSN and returns a FS
+// DSN format: <srv>://<repo url>#<ref> (see github.com/hairyhenderson/go-fsimpl)
+// Example git+file:///Users/bruno.silva/git/personal/templates
 func New(dsn string) (fs.FS, error) {
 	mux := fsimpl.NewMux()
 	mux.Add(gitfs.FS)
